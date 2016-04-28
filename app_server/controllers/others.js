8f60ca562ebb4a8a3e12490334f1567c0503d08e
User <user@air-di-user.rot.it.ibm.com>
2016-04-28 13:54:11 +0200

    data moved to contollers

--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -1,3 +1,6 @@
 module.exports.about = function(req, res){
-res.render('generic-text', { title: 'About' });
+	res.render('generic-text', { 
+		title: 'About Loc8r',
+		content: 'Loc8r was created to help people find places to sit down and get a bit of work done.'
+	});
 };
